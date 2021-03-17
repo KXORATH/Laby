acb1fb8bea20199e32cd5ccbaaddf608cdad7205
KXORATH <minecraftKXORATH@gmail.com>
2021-03-17 20:30:31 +0100

    8.1

--- Lab2_studia/js/walidacja.js
+++ Lab2_studia/js/walidacja.js
@@ -1,18 +1,11 @@
-function sprawdzPole(pole_id,obiektRegex) {
-//Funkcja sprawdza czy wartość wprowadzona do pola tekstowego
-//pasuje do wzorca zdefiniowanego za pomocą wyrażenia regularnego
-//Parametry funkcji:
-//pole_id - id sprawdzanego pola tekstowego
-//obiektRegex - wyrażenie regularne
-//---------------------------------
+function sprawdzPole(pole_id,obiektRegex)
+{
     var obiektPole = document.getElementById(pole_id);
-    if(!obiektRegex.test(obiektPole.value)) return (false);
-    else return (true);
+    if(!obiektRegex.test(obiektPole.value)) return false;
+    else return true;
 }
-function sprawdz_radio(nazwa_radio){
-//Funkcja sprawdza czy wybrano przycisk radio
-//z grupy przycisków o nazwie nazwa_radio
-//---------------------------------------
+function sprawdz_radio(nazwa_radio)
+{
     var obiekt=document.getElementsByName(nazwa_radio);
     for (i=0;i<obiekt.length;i++)
     { wybrany=obiekt[i].checked;
@@ -20,31 +13,51 @@ function sprawdz_radio(nazwa_radio){
     return false;
 }
 function sprawdz_box(box_id)
-{//Funkcja sprawdza czy przycisk typu checkbox
-//o identyfikatorze box_id jest zaznaczony
-//----------------------------------------
-    var obiekt=document.getElementById(box_id);
-    if (obiekt.checked) return true;
-    else return false;
+{
+    var obiekt=document.getElementsByName(box_id);
+    for (i=0;i<obiekt.length;i++)
+    { wybrany=obiekt[i].checked;
+        if (wybrany) return true; }
+    return false;
 }
 function sprawdz()
-{ //Funkcja realizujaca sprawdzanie całego fomularza
-//wykorzystując funkcje pomocnicze
-//--------------------------------
-    var ok=true; //zmienna informująca o poprawnym wypełnieniu formularza
-//Definicje odpowiednich wyrażeń regularnych dla sprawdzenia
-//poprawności danych wprowadzonych do pól tekstowych
-    obiektNazw = /^[a-zA-Z]{2,20}$/; //wyrażenie regularne dla nazwiska
+{
+    var ok=true;
+    obiektNazw = /^[a-zA-Z]{2,20}$/;
     obiektemail = /^([a-zA-Z0-9])+([.a-zA-Z0-9_-])*@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-]+)+/;
     obiektWiek=/^[1-9][0-9]{1,2}$/;
-//Sprawdzanie kolejnych pól formularza.
-//w przypadku błędu - pojawia się odpowiedni komunikat
+
     if (!sprawdzPole("nazw",obiektNazw))
-    { ok=false;
+    {
+        ok=false;
         document.getElementById("nazw_error").innerHTML= "Wpisz poprawnie nazwisko!";
     }
     else document.getElementById("nazw_error").innerHTML="";
-//Uzupełnij – sprawdz kolejne pola formularza
-// if ...
+    if (!sprawdzPole("wiek",obiektWiek))
+    {
+        ok=false;
+        document.getElementById("wiek_error").innerHTML= "Wpisz poprawnie wiek!";
+    }
+    else document.getElementById("wiek_error").innerHTML="";
+    if (!sprawdzPole("email",obiektemail))
+    {
+        ok=false;
+        document.getElementById("email_error").innerHTML= "Wpisz poprawnie email!";
+    }
+    else document.getElementById("email_error").innerHTML="";
+    if (!sprawdz_box("jezyk"))
+    {
+        ok=false;
+        document.getElementById("produkt_error").innerHTML= "Zaznacz produkt!";
+    }
+    else document.getElementById("produkt_error").innerHTML="";
+    if (!sprawdz_radio("zaplata"))
+    {
+        ok=false;
+        document.getElementById("zaplata_error").innerHTML= "Wybierz sposób zapłaty!";
+    }
+    else document.getElementById("zaplata_error").innerHTML="";
     return ok;
+
+
 }
