cf9f0d53bb8a9e32d63870a68055a92b61c9897d
KXORATH <minecraftKXORATH@gmail.com>
2021-04-01 20:59:53 +0200

    Zrobione poprawki

--- Lab2_studia/js/main.js
+++ Lab2_studia/js/main.js
@@ -3,8 +3,24 @@ function oblicz()
   var k=parseInt(document.getElementById('k').value);
   var n=parseInt(document.getElementById('n').value);
   var pr=parseInt(document.getElementById('pr').value);
-  var r=document.getElementById('r');
-  var ko=document.getElementById('ko');
   r.value=((k*pr/12/100)/(1-(1/Math.pow(1+pr/100/12,n)))).toFixed(2);
   ko.value=(n*r.value).toFixed(2);
+  if(isNaN(r)||isFinite(r))
+  {
+    r.value = "Błędne dane";
+    r=document.getElementById('r');
+  }
+  else
+  {
+      r=document.getElementById('r');
+  }
+  if(isNaN(ko)||isFinite(ko))
+  {
+    ko.value = "Błędne dane";
+    ko=document.getElementById('ko');
+  }
+  else
+  {
+    ko=document.getElementById('ko');
+  }
 }
